b535873c76f81cf0b630ce5f3fa9e4dc43e43e3c
g4ngli0s <juanker.cracker@gmail.com>
2020-01-18 08:09:30 +0100

    Create dllsimple.h

---
+++ dllinjection/dllsimple.h
@@ -0,0 +1,23 @@
+#ifndef _DLL_H_
+#define _DLL_H_
+
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
+#define DLL_MSG __cdecl
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+DLL_EXPORT void MsgDll();
+
+#ifdef __cplusplus
+} // __cplusplus defined.
+#endif
+#endif
